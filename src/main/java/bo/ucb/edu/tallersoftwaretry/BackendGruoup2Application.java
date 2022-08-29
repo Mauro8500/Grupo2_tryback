e24078681ec733eea2507a52233f96615f72f199
HP@DESKTOP-H3RGHQJ <mauricioillanes17@gmail.com>
2022-08-29 00:29:18 -0400

    first commit

---
+++ src/main/java/bo/ucb/edu/tallersoftwaretry/BackendGruoup2Application.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.tallersoftwaretry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGruoup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendGruoup2Application.class, args);
+	}
+
+}
---
+++ src/test/java/bo/ucb/edu/tallersoftwaretry/BackendGruoup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.ucb.edu.tallersoftwaretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGruoup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
